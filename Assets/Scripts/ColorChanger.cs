7f223f645e9b99c8ca9cd3cad7ac5d3f53b1093a
Adrian <59284453+adrianrabei@users.noreply.github.com>
2020-02-04 18:07:11 +0200

    Pin controll, color changer

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    [SerializeField] private Material matGreen;
+
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.tag == "Colored") {
+            GetComponent<Renderer>().material = matGreen;
+            transform.gameObject.tag = "Colored";
+        }
+    }
+}
---
+++ Assets/Scripts/PinControll.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinControll : MonoBehaviour
+{
+    private Rigidbody pin;
+    private float speed = 25000f;
+    private Collider coll;
+    private float timeLeft = 1.3f;
+    private bool destruction;
+    void Start()
+    {
+        pin = GetComponent<Rigidbody>();
+        coll = GetComponent<Collider>();
+        destruction = false;
+    }
+
+    void FixedUpdate()
+    {
+       if(destruction){
+            timeLeft -= Time.deltaTime;
+            if(timeLeft < 1)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    private void OnMouseDown() {
+        pin.AddForce(transform.right * speed);
+        coll.enabled = false;
+        destruction = true;
+    }
+
+    
+}
